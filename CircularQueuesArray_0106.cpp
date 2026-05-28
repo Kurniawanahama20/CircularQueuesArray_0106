0c0ecf29fce1c8356c32f4c9a373e687e880ee74
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-05-28 11:08:06 +0700

    Menambahkan menu utama pada program queue

--- CircularQueuesArray_0106.cpp
+++ CircularQueuesArray_0106.cpp
@@ -122,6 +122,23 @@ public:
 
 int main()
 {
-    
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values " << endl;
+            cout << "4.exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+        }
+    }
 }
 
